3dcb130d4fe8aedec85ad7c8f7b264ef81ba3147
Christopher Eby <kreed@kreed.org>
2017-10-15 18:35:08 -0500

    fix nav highlight

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const App = () => {
 			<div className='flex-container'>
 				<Menu inverted compact attached='top'>
 					<Responsive as={Menu.Item} header minWidth={700}>OpenStreetMap power infrastructure</Responsive>
-					<Menu.Item as={NavLink} activeClassName="active" to="/">Map</Menu.Item>
+					<Menu.Item as={NavLink} activeClassName="active" to="/" exact>Map</Menu.Item>
 					<Menu.Item as={NavLink} activeClassName="active" to="/tpit">ERCOT project tracking</Menu.Item>
 					<Menu.Item as={NavLink} activeClassName="active" to="/about">About</Menu.Item>
 				</Menu>
