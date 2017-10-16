86c37c3be978dec6f4b22bdaaadd668688c77147
Christopher Eby <kreed@kreed.org>
2017-10-15 21:31:49 -0500

    only include required semantic css modules

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'leaflet/dist/leaflet.css';
-import 'semantic-ui-css/semantic.min.css';
+import './semantic.css';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
