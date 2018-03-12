bda1d5ac126a5e7bf9a6a7c971b269260be26b03
Christopher Eby <kreed@kreed.org>
2018-03-12 05:09:14 -0500

    back to mapbox-gl
    style still needs work to restore all the features from the tangram
    version

--- src/Map.js
+++ src/Map.js
@@ -1,88 +1,42 @@
-import React from 'react';
-import L from 'leaflet';
-import 'leaflet-hash';
+import React from 'react'
+import mapboxgl from 'mapbox-gl'
+import PowerStyle from './powerstyle'
 
-// -- Import Tangram --
+mapboxgl.accessToken = 'pk.eyJ1Ijoia3JlM2QiLCJhIjoiY2lwcjJrZ3p2MDZyOWZvbTN0bXV1eXF6ZiJ9.xXv_HMXLK04bEtmuwsEyMQ';
 
-// You can import the main library, but it takes forever to bundle, because
-// it is already minified.
-// import Tangram from 'tangram';
-
-// This is faster, and will be minified anyway in production
-import Tangram from 'tangram/dist/tangram.debug';
-
-// -- LEAFLET: Fix Leaflet's icon paths for Webpack --
-// See here: https://github.com/PaulLeCam/react-leaflet/issues/255
-// Used in conjunction with url-loader.
-delete L.Icon.Default.prototype._getIconUrl;
-L.Icon.Default.mergeOptions({
-	iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-	iconUrl: require('leaflet/dist/images/marker-icon.png'),
-	shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
-});
-
-// NOTE: This uses React to create a "wrapper" component around an HTML element
-// that Leaflet hooks into. This means you need to manage Leaflet's state
-// manually, which may not be what you want to work in a pure React way.
-// As an alternative, you may want to check out https://github.com/PaulLeCam/react-leaflet
-
-export default class Map extends React.Component {
-	componentDidMount() {
-		var map_start_location = [30.55, -98.34, 6];
-		const url_hash = window.location.hash.slice(1, window.location.hash.length).split('/');
-
-		if (url_hash.length === 3) {
-			map_start_location = [url_hash[1],url_hash[2],url_hash[0]];
-			// convert from strings
-			map_start_location = map_start_location.map(Number);
-		}
-
-		const map = L.map(this.mapEl, { zoomControl: false });
-		map.attributionControl.setPrefix(false);
-		const layer = Tangram.leafletLayer({
-			scene: process.env.PUBLIC_URL + '/scene.yaml',
-			attribution: '&copy; <a href="//www.openstreetmap.org/">OpenStreetMap contributors</a>'
-		});
-
-		function onMapClick(selection) {
-			if (selection.feature) {
-				var latlng = selection.leaflet_event.latlng;
-				L.popup()
-					.setLatLng(latlng)
-					.setContent(selection.feature.properties.description)
-					.openOn(map);
-			}
-		}
-
-		layer.addTo(map);
-		layer.on('init', function() {
-			layer.setSelectionEvents({
-				click: onMapClick
-			});
-		});
-		this._layer = layer;
-
-		map.setView(map_start_location.slice(0, 3), map_start_location[2]);
-
-		new L.Hash(map);
-	}
+let Map = class Map extends React.Component {
+	map;
 
 	componentWillReceiveProps(nextProps) {
-		if (this._layer && this._layer.scene && this._layer.scene.config) {
-			const scene = this._layer.scene;
-			scene.config.layers['power-line'].enabled = nextProps.lines;
-			scene.config.global.ercotgrid = nextProps.grid;
-			scene.config.layers.ercotrtm.enabled = nextProps.rtm;
-			scene.updateConfig();
+		if (this.style) {
+			if (this.props.lines != nextProps.lines || this.props.grid != nextProps.grid)
+				this.style.set_show_powerlines(nextProps.lines, nextProps.grid);
+			if (this.props.rtm != nextProps.rtm)
+				this.style.set_ercot_rtm(nextProps.rtm);
 		}
+	}
+
+	componentDidMount() {
+		this.map = new mapboxgl.Map({
+			container: this.mapContainer,
+			style: 'mapbox://styles/mapbox/light-v9',
+			hash: true,
+			center: [-98.34, 30.55],
+			zoom: 6
+		});
+
+		this.map.on('load', () => {
+			this.style = new PowerStyle(this.map, "https://power.kreed.org/tiles/power/{z}/{x}/{y}.pbf");
+		});
 	}
 
 	render() {
 		return (
-			<div ref={(ref) => {this.mapEl = ref }}>
+			<div ref={el => this.mapContainer = el}>
 				{this.props.children}
 			</div>
 		);
 	}
 }
 
+export default Map;
---
+++ src/powerstyle.js
@@ -0,0 +1,504 @@
+import mapboxgl from 'mapbox-gl'
+
+var colors = {
+	"#ccc": ["none"],
+	"#444": ["unknown"],
+	"#75fff3": ["HVDC"],
+	"#d07b87": ["1000000"],
+	"#882e72": ["800000"],
+	"#c640d9": ["765000", "750000"],
+	"#d659b7": ["735000", "660000"],
+	"#563389": ["525000", "500000", "450000"],
+	"#a1a442": ["440000", "420000", "400000"],
+	"#cb87a1": ["380000", "360000", "350000"],
+	"#c23e61": ["345000", "330000", "315000", "300000"],
+	"#e8601c": ["287000", "275000"],
+	"#f1932d": ["240000", "230000", "225000"],
+	"#cd4938": ["220000", "187000", "170000"],
+	"#1965b0": ["161000", "154000", "150000", "144000", "138000", "132000", "130000"],
+	"#7bafde": ["125000", "120000", "115000", "113000", "110000", "100000", "90000", "77000"],
+	"#4eb265": ["72000", "70000", "69000", "66000", "65000", "63000"],
+	"#818e38": ["60000", "55000", "50000", "46000"],
+	"#7e492d": ["35000", "34500", "33000", "22000", "20000"],
+};
+
+var voltage_colors = {};
+for (var color in colors) {
+	var voltages = colors[color];
+	for (var i = 0; i < voltages.length; ++i) {
+		voltage_colors[voltages[i]] = color;
+	}
+}
+
+var voltage_combos = [
+	'500000;230000',
+	'500000;115000',
+	'400000;275000',
+	'400000;225000',
+	'400000;220000',
+	'400000;150000',
+	'400000;110000',
+	'380000;220000;110000',
+	'380000;220000',
+	'380000;132000',
+	'380000;110000',
+	'345000;230000',
+	'345000;161000',
+	'345000;138000',
+	'345000;115000',
+	'345000;69000',
+	'275000;154000',
+	'275000;66000',
+	'230000;34500',
+	'230000;138000',
+	'230000;115000',
+	'230000;69000',
+	'220000;150000',
+	'220000;132000',
+	'220000;115000',
+	'220000;110000',
+	'220000;66000',
+	'161000;69000',
+	'154000;66000',
+	'150000;70000',
+	'150000;60000',
+	'138000;69000',
+	'132000;66000',
+	'132000;55000',
+	'132000;50000',
+	'115000;69000',
+	'115000;34500',
+	'110000;20000',
+	'66000;22000'
+];
+
+let PowerStyle = class PowerStyle {
+	labels() {
+		this.map.addLayer({
+			"id": "powerline label",
+			"type": "symbol",
+			"source": "power",
+			"source-layer": "power-line",
+			"filter": ["in", "kind", "line", "cable"],
+			"minzoom": 10,
+			"layout": {
+				"text-size": {
+					"base": 1,
+					"stops": [[9,10], [20, 16]]
+				},
+				"text-max-angle": 30,
+				"symbol-spacing": 100,
+				"text-font": [
+					"DIN Offc Pro Medium",
+					"Arial Unicode MS Regular"
+				],
+				"symbol-placement": "line",
+				"text-padding": 1,
+				"text-offset": [0,0],
+				"text-rotation-alignment": "map",
+				"text-anchor": "bottom",
+				"text-field": "{label}"
+			},
+			"paint": {
+				"text-halo-color": "#fff",
+				"text-color": "hsl(0, 1%, 34%)",
+				"text-halo-width": {
+					 "base": 1,
+					 "stops": [[14,1.25],[15,1.5]]
+				},
+				"text-halo-blur": 0,
+				"text-translate": [0,-2]
+			}
+		});
+		this.map.addLayer({
+			"id": "generator label",
+			"type": "symbol",
+			"source": "power",
+			"source-layer": "power-point",
+			"filter": [
+				"all",
+				["==", "kind", "generator"],
+			],
+			"minzoom": 16,
+			"layout": {
+				"text-line-height": 1,
+				"text-size": {
+					"base": 1,
+					"stops": [[12,11],[16,16]]
+				},
+				"symbol-avoid-edges": true,
+				"text-transform": "uppercase",
+				"text-font": [
+					"DIN Offc Pro Medium",
+					"Arial Unicode MS Regular"
+				],
+				"symbol-placement": "point",
+				"text-padding": 3,
+				"text-field": "{name} {generator:output:electricity}",
+				"text-letter-spacing": 0.1,
+				"text-max-width": 7
+			},
+			"paint": {
+				"text-halo-color": "#fff",
+				"text-color": "hsl(0, 1%, 34%)",
+				"text-halo-width": {
+					 "base": 1,
+					 "stops": [[14,1.25],[15,1.5]]
+				},
+				"text-halo-blur": 0,
+				"text-translate": [0,-2]
+			}
+		});
+		this.map.addLayer({
+			"id": "substation label",
+			"type": "symbol",
+			"source": "power",
+			"source-layer": "power-point",
+			"filter": [
+				"all",
+				["in", "kind", "substation"],
+			],
+			"minzoom": 10,
+			"layout": {
+				"text-line-height": 1,
+				"text-size": {
+					"base": 1,
+					"stops": [[12,11],[16,16]]
+				},
+				"symbol-avoid-edges": true,
+				"text-transform": "uppercase",
+				"text-font": [
+					"DIN Offc Pro Medium",
+					"Arial Unicode MS Regular"
+				],
+				"symbol-placement": "point",
+				"text-padding": 3,
+				"text-field": "{name}",
+				"text-letter-spacing": 0.1,
+				"text-max-width": 7
+			},
+			"paint": {
+				"text-halo-color": "#fff",
+				"text-color": "hsl(0, 1%, 34%)",
+				"text-halo-width": {
+					 "base": 1,
+					 "stops": [[14,1.25],[15,1.5]]
+				},
+				"text-halo-blur": 0,
+				"text-translate": [0,-2]
+			}
+		});
+		this.map.addLayer({
+			"id": "plant label",
+			"type": "symbol",
+			"source": "power",
+			"source-layer": "power-point",
+			"filter": [
+				"all",
+				["in", "kind", "plant"],
+			],
+			"layout": {
+				"text-line-height": 1,
+				"text-size": {
+					"base": 1,
+					"stops": [[6,11],[16,16]]
+				},
+				"text-font": ["Open Sans Bold", "Arial Unicode MS Bold"],
+				"symbol-placement": "point",
+				"text-padding": 3,
+				"text-field": "{label}",
+				"text-letter-spacing": 0.1,
+				"text-max-width": 7
+			},
+			"paint": {
+				"text-halo-color": "#fff",
+				"text-color": "hsl(0, 1%, 34%)",
+				"text-halo-width": {
+					 "base": 1,
+					 "stops": [[14,1.25],[15,1.5]]
+				},
+				"text-halo-blur": 0,
+				"text-translate": [0,-2]
+			},
+			"minzoom": 6
+		});
+	}
+
+	ercot_rtm() {
+		this.map.addSource('ercot_rtm', {
+			'type': 'geojson',
+			'data': 'ercot_rtm.geojson'
+		});
+		this.map.addLayer({
+			"id": "ercot_rtm",
+			"type": "circle",
+			"source": "ercot_rtm",
+			"paint": {
+				'circle-color': '#555',
+				'circle-radius': {
+					"stops": [[5, 0], [6, 8]]
+				},
+			}
+		});
+		this.map.on('click', function (e) {
+			var features = this.map.queryRenderedFeatures(e.point, { layers: ['ercot_rtm'] });
+
+			if (!features.length) {
+				return;
+			}
+
+			var feature = features[0];
+
+			// Populate the popup and set its coordinates
+			// based on the feature found.
+			new mapboxgl.Popup()
+				.setLngLat(feature.geometry.coordinates)
+				.setHTML(feature.properties.description)
+				.addTo(this.map);
+		});
+	}
+
+	set_ercot_rtm(visible) {
+		if (!this.map.getSource('ercot_rtm'))
+			this.ercot_rtm();
+		this.map.setPaintProperty('ercot_rtm', 'circle-opacity', visible ? 1 : 0);
+	}
+
+	powerline_group(pfx, base) {
+		if (window.location.search) {
+			var v = window.location.search.substring(1);
+			var comb = voltage_combos.filter(function (e) { return e.indexOf(v) >= 0 });
+			base.filter.push(["in", "voltage", v].concat(comb));
+		}
+
+		var cable_base = JSON.parse(JSON.stringify(base));
+		cable_base.filter.push(["==", "kind", "cable"]);
+		cable_base.paint['line-dasharray'] = [2, 2];
+
+		var cable_novoltage = JSON.parse(JSON.stringify(cable_base));
+		cable_novoltage.filter.push(["!has", "voltage"]);
+		cable_novoltage.id = pfx + "cable no voltage";
+		cable_novoltage.paint['line-color'] = voltage_colors.none;
+		cable_novoltage.minzoom = 9;
+		this.map.addLayer(cable_novoltage, "powerline label");
+
+		var cable = JSON.parse(JSON.stringify(cable_base));
+		cable.id = pfx + "power cable";
+		cable.filter = cable.filter.concat([
+			["has", "voltage"],
+			["!=", "frequency", "0"],
+		]);
+		var max_stops = [];
+		max_stops.push([0, voltage_colors.unknown])
+		for (var e in voltage_colors) {
+			var n = Number(e);
+			if (n > 0) {
+				max_stops.push([n, voltage_colors[e]]);
+			}
+		}
+		cable.paint["line-color"] = {
+			"property": "max_voltage",
+			"type": "categorical",
+			"stops": max_stops
+		}
+		this.map.addLayer(cable, "powerline label");
+
+		var cable_hvdc = JSON.parse(JSON.stringify(cable_base));
+		cable_hvdc.id = pfx + "cable_hvdc";
+		cable_hvdc.filter = cable_hvdc.filter.concat([
+			["has", "voltage"],
+			["==", "frequency", "0"],
+		]);
+		cable_hvdc.paint["line-color"] = voltage_colors.HVDC;
+		this.map.addLayer(cable_hvdc, "powerline label");
+
+		base.filter.push(["==", "kind", "line"]);
+
+		var novoltage = JSON.parse(JSON.stringify(base));
+		novoltage.filter.push(["!has", "voltage"]);
+		novoltage.id = pfx + "line_no_voltage";
+		novoltage.paint['line-color'] = voltage_colors.none;
+		novoltage.minzoom = 9;
+		this.map.addLayer(novoltage, "powerline label");
+
+		var primary = JSON.parse(JSON.stringify(base));
+		primary.filter.push(["in", "voltage"].concat(voltage_combos));
+		primary.paint["line-color"] = {
+			"property": "voltage",
+			"type": "categorical"
+		}
+		var secondary = JSON.parse(JSON.stringify(primary));
+		var primary_stops = [['unknown', voltage_colors.unknown]];
+		var secondary_stops = [['unknown', voltage_colors.unknown]];
+		for (var i = 0; i < voltage_combos.length; ++i) {
+			var voltages = voltage_combos[i].split(';');
+			if (!voltage_colors.hasOwnProperty(voltages[0])) console.log('missing', voltages[0]);
+			if (!voltage_colors.hasOwnProperty(voltages[1])) console.log('missing', voltages[1]);
+			primary_stops.push([voltage_combos[i], voltage_colors[voltages[0]]]);
+			secondary_stops.push([voltage_combos[i], voltage_colors[voltages[1]]]);
+		}
+		primary.id = pfx + 'powerline primary';
+		primary.paint['line-color'].stops = primary_stops;
+		primary.paint['line-dasharray'] = [2, 2];
+		secondary.id = pfx + 'powerline secondary';
+		secondary.paint['line-color'].stops = secondary_stops;
+		secondary.paint['line-dasharray'] = [0, 2, 2];
+		this.map.addLayer(primary, "powerline label");
+		this.map.addLayer(secondary, "powerline label");
+
+		var line = JSON.parse(JSON.stringify(base));
+		line.id = pfx + "power line";
+		line.filter = line.filter.concat([
+			["has", "voltage"],
+			["!in", "voltage"].concat(voltage_combos),
+			["!=", "frequency", "0"],
+		]);
+		var stops = [];
+		stops.push(['unknown', voltage_colors.unknown])
+		for (var e in voltage_colors) {
+			var n = Number(e);
+			if (n > 0) {
+				stops.push([e, voltage_colors[e]]);
+				stops.push([e + ';0', voltage_colors[e]]);
+			}
+		}
+		line.paint["line-color"] = {
+			"property": "voltage",
+			"type": "categorical",
+			"stops": stops
+		}
+		this.map.addLayer(line, "powerline label");
+
+		var hvdc = JSON.parse(JSON.stringify(base));
+		hvdc.id = pfx + "hvdc";
+		hvdc.filter = hvdc.filter.concat([
+			["has", "voltage"],
+			["==", "frequency", "0"],
+		]);
+		hvdc.paint["line-color"] = voltage_colors['HVDC']
+		this.map.addLayer(hvdc, "powerline label");
+	}
+
+	powerlines() {
+		var base = {
+			"type": "line",
+			"source": "power",
+			"source-layer": "power-line",
+			"filter": [
+				"all",
+				["!=", "grid", "ercot"],
+			],
+			"layout": {
+				"line-join": "miter"
+			},
+			"paint": {
+				"line-width": {
+					"base": 1.5,
+					"stops": [[8,1.5],[12,3]]
+				},
+			}
+		}
+		this.powerline_group('otherline_', base);
+
+		base = {
+			"type": "line",
+			"source": "power",
+			"source-layer": "power-line",
+			"filter": [
+				"all",
+				["==", "grid", "ercot"],
+			],
+			"layout": {
+				"line-join": "miter"
+			},
+			"paint": {
+				"line-width": {
+					"base": 1.5,
+					"stops": [[8,1.5],[12,3]]
+				},
+			}
+		}
+		this.powerline_group('ercotline_', base);
+	}
+
+	power_areas() {
+		this.map.addLayer({
+			"id": "power area",
+			"type": "line",
+			"source": "power",
+			"source-layer": "power-polygon",
+			"filter": [
+				"all",
+				["in", "kind", "substation", "plant"],
+			],
+			"minzoom": 10,
+			"paint": {
+				"line-color": "hsl(0, 1%, 34%)",
+				"line-width": 1
+			}
+		});
+		this.map.addLayer({
+			"id": "generator polygon",
+			"source": "power",
+			"source-layer": "power-polygon",
+			"filter": [
+				"all",
+				["==", "kind", "generator"],
+				["==", "$type", "Polygon"],
+			],
+			"type": "fill",
+			"paint": {
+				'fill-color': 'hsl(0, 100%, 71%)',
+				'fill-outline-color': 'hsl(0, 100%, 32%)',
+			}
+		});
+		this.map.addLayer({
+			"id": "generator point",
+			"source": "power",
+			"source-layer": "power-point",
+			"filter": [
+				"all",
+				["==", "kind", "generator"],
+				["!=", "label_placement", true],
+			],
+			"type": "circle",
+			"paint": {
+				'circle-color': 'hsl(0, 100%, 71%)',
+				'circle-stroke-color': 'hsl(0, 100%, 32%)',
+				'circle-stroke-width': {
+					"stops": [[5, 0], [7, 0.5], [12, 3]]
+				},
+				'circle-radius': {
+					"stops": [[5, 0], [7, 0.5], [12, 4]]
+				},
+			}
+		});
+	}
+
+	set_show_powerlines(show, highlight_ercot) {
+		for (var layer in this.map.style._layers) {
+			if (layer.startsWith('otherline_')) {
+				this.map.setPaintProperty(layer, 'line-opacity', show ? (highlight_ercot ? 0.33 : 1) : 0);
+			} else if (layer.startsWith('ercotline_')) {
+				this.map.setPaintProperty(layer, 'line-opacity', show ? 1 : 0);
+			}
+		}
+	}
+
+	constructor(map, source) {
+		this.map = map;
+
+		map.addSource('power', {
+			type: 'vector',
+			tiles: [source],
+			minzoom: 0,
+			maxzoom: 16
+		});
+
+		this.power_areas();
+		this.labels();
+		this.powerlines();
+	}
+}
+
+export default PowerStyle;
