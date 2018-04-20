74c2ec7cb7bde71e72186d6fbe43cff5b3c776b8
Christopher Eby <kreed@kreed.org>
2018-04-20 02:06:14 -0500

    make the year for either side configurable

--- src/Compare.js
+++ src/Compare.js
@@ -1,43 +1,55 @@
 import React from 'react'
 import Map from './Map'
 import mbglCompare from 'mapbox-gl-compare'
-import { Dropdown, Segment } from 'semantic-ui-react'
+import { Dropdown } from 'semantic-ui-react'
 
 const yearOptions = [
 	{text: '2014', value: '2014'},
 	{text: '2015', value: '2015'},
 	{text: '2016', value: '2016'},
-	{text: '2017', value: '2017'}
+	{text: '2017', value: '2017'},
+	{text: 'Now', value: 'now'}
 ];
+
+function yearUrl(year) {
+	if (year === 'now') return null;
+	return 'https://power.kreed.org/' + year + '/power' + year + '/{z}/{x}/{y}.pbf'
+}
 
 export default class Compare extends React.Component {
 	state = {
-		year: '2014'
+		leftYear: '2014',
+		rightYear: 'now'
 	}
 
-	changeYear = (e, data) => {
+	leftYear = (e, data) => {
 		this.setState(prevState => ({
-			year: data.value
+			leftYear: data.value
+		}));
+	}
+
+	rightYear = (e, data) => {
+		this.setState(prevState => ({
+			rightYear: data.value
 		}));
 	}
 
 	componentDidMount() {
-		new mbglCompare(this.oldMap.map, this.newMap.map);
+		this.mbglcmp = new mbglCompare(this.leftMap.map, this.rightMap.map);
 	}
 
 	componentDidUpdate(prevProps, prevState, snapshot) {
-		new mbglCompare(this.oldMap.map, this.newMap.map);
+		this.mbglcmp._container.remove();
+		this.mbglcmp = new mbglCompare(this.leftMap.map, this.rightMap.map);
 	}
 
 	render() {
 		return (
 			<div className='compare-container'>
-				<Map ref={el => this.oldMap = el} lines={true} tileUrl={'https://power.kreed.org/' + this.state.year + '/power' + this.state.year + '/{z}/{x}/{y}.pbf'} key={this.state.year} />
-				<Map ref={el => this.newMap = el} lines={true} />
-				<Segment inverted className='map-control'>
-					<Dropdown selection options={yearOptions} onChange={this.changeYear} value={this.state.year} />
-				</Segment>
-				<Segment inverted className='map-control right'>Now</Segment>
+				<Map ref={el => this.leftMap = el} lines={true} tileUrl={yearUrl(this.state.leftYear)} key={'left' + this.state.leftYear} />
+				<Map ref={el => this.rightMap = el} lines={true} tileUrl={yearUrl(this.state.rightYear)} key={'right' + this.state.rightYear} />
+				<Dropdown className='map-control' compact selection options={yearOptions} onChange={this.leftYear} value={this.state.leftYear} />
+				<Dropdown className='map-control right' compact selection options={yearOptions} onChange={this.rightYear} value={this.state.rightYear} />
 			</div>
 		);
 	}
