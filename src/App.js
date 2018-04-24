6a95a6b18c19d02c9985cb738f931ed06fc5705b
Christopher Eby <kreed@kreed.org>
2018-04-24 15:13:48 -0500

    make navlink work with hash

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { matchPath } from 'react-router';
 import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 import { Menu, Responsive } from 'semantic-ui-react'
 
@@ -6,6 +7,14 @@ import About from './About';
 import Home from './Home';
 import Compare from './Compare';
 import TPIT from './TPIT';
+
+const MyNavLink = ({exact, ...rest}) => {
+	const isActive = (match, location) => {
+		// ignore hash when matching url (also discards search params as a side effect)
+		return matchPath(location.pathname, {path: rest.to.replace(/#.*/, ''), exact});
+	}
+	return <NavLink activeClassName="active" isActive={isActive} {...rest} />;
+}
 
 export default class App extends React.Component {
 	state = {
@@ -23,10 +32,10 @@ export default class App extends React.Component {
 			<div className='flex-container'>
 				<Menu inverted compact attached='top'>
 					<Responsive as={Menu.Item} header minWidth={700}>OpenStreetMap power infrastructure</Responsive>
-					<Menu.Item as={NavLink} activeClassName="active" to={'/' + this.state.hash} exact>Map</Menu.Item>
-					<Menu.Item as={NavLink} activeClassName="active" to={'/compare' + this.state.hash}>Then and now</Menu.Item>
-					<Menu.Item as={NavLink} activeClassName="active" to={'/tpit' + this.state.hash}>ERCOT projects</Menu.Item>
-					<Menu.Item as={NavLink} activeClassName="active" to="/about">About</Menu.Item>
+					<Menu.Item as={MyNavLink} to={'/' + this.state.hash} exact>Map</Menu.Item>
+					<Menu.Item as={MyNavLink} to={'/compare' + this.state.hash}>Then and now</Menu.Item>
+					<Menu.Item as={MyNavLink} to={'/tpit' + this.state.hash}>ERCOT projects</Menu.Item>
+					<Menu.Item as={MyNavLink} to="/about">About</Menu.Item>
 				</Menu>
 				<Route exact path="/" render={props => ( <Home onMapMove={this.mapMove} /> )} />
 				<Route path="/tpit" render={props => ( <TPIT onMapMove={this.mapMove} /> )} />
